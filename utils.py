103501ed03a66a7589b483eb4b00b2d9262a8b00
JoseSom <joseluissom004@gmail.com>
2023-03-13 19:13:35 -0600

    Se agregan el menu, metodo para obtener canciones, para imprimir las canciones obtenidas y se crea el archivo de utils

---
+++ utils.py
@@ -0,0 +1,10 @@
+def divisor():
+    return print("------------------------------------------------------------")
+
+
+def welcome():
+    print("Bienvenido")
+    print(""" Menu
+    1. Mostrar Top 10 de canciones por artista
+    0. Salir
+    """)
